463a5153196da0293842f6388499ded445985d64
ChabVlad <vlad.chabak@gmail.com>
2024-09-06 14:53:04 +0400

    BookDto,BookController, BookMapper was added

--- src/main/java/project/bookstore/controller/BookController.java
+++ src/main/java/project/bookstore/controller/BookController.java
@@ -3,7 +3,9 @@ package project.bookstore.controller;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import project.bookstore.dto.BookDto;
@@ -23,13 +25,13 @@ public class BookController {
         return bookService.findAll();
     }
 
-    @GetMapping
-    public BookDto getBookById(Long id) {
+    @GetMapping("/{id}")
+    public BookDto getBookById(@PathVariable Long id) {
         return bookService.getBookById(id);
     }
 
     @PostMapping
-    public BookDto createBook(CreateBookRequestDto requestBookDto) {
+    public BookDto createBook(@RequestBody CreateBookRequestDto requestBookDto) {
         return bookMapper.toDto(bookService.save(requestBookDto));
     }
 }
--- src/main/java/project/bookstore/repository/BookRepository.java
+++ src/main/java/project/bookstore/repository/BookRepository.java
@@ -1,7 +1,6 @@
 package project.bookstore.repository;
 
 import java.util.List;
-import project.bookstore.dto.BookDto;
 import project.bookstore.model.Book;
 
 public interface BookRepository {
