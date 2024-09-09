14183874ce85e20db4fed94a1fc67b07a8e4088d
ChabVlad <vlad.chabak@gmail.com>
2024-09-09 15:15:17 +0400

    @Repository removed

--- src/main/java/project/bookstore/repository/BookRepository.java
+++ src/main/java/project/bookstore/repository/BookRepository.java
@@ -1,10 +1,8 @@
 package project.bookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import project.bookstore.model.Book;
 
-@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
