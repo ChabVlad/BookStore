a8a8bdb073121f6d5799a258f265a79b3f19ef5a
ChabVlad <vlad.chabak@gmail.com>
2024-09-20 14:40:03 +0400

    done

--- src/main/java/project/bookstore/service/impl/UserServiceImpl.java
+++ src/main/java/project/bookstore/service/impl/UserServiceImpl.java
@@ -1,12 +1,17 @@
 package project.bookstore.service.impl;
 
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import project.bookstore.dto.user.UserRegistrationRequestDto;
 import project.bookstore.dto.user.UserResponseDto;
 import project.bookstore.exception.RegistrationException;
 import project.bookstore.mapper.UserMapper;
+import project.bookstore.model.Role;
+import project.bookstore.model.RoleName;
 import project.bookstore.model.User;
+import project.bookstore.repository.role.RoleRepo;
 import project.bookstore.repository.user.UserRepo;
 import project.bookstore.service.UserService;
 
@@ -15,6 +20,8 @@ import project.bookstore.service.UserService;
 public class UserServiceImpl implements UserService {
     private final UserMapper userMapper;
     private final UserRepo userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepo roleRepository;
 
     @Override
     public UserResponseDto register(UserRegistrationRequestDto requestDto) {
@@ -22,6 +29,9 @@ public class UserServiceImpl implements UserService {
             throw new RegistrationException(requestDto.getEmail() + "allready exists!");
         }
         User user = userMapper.toModel(requestDto);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        Role userRole = roleRepository.findByRole(RoleName.ROLE_USER);
+        user.setRoles(Set.of(userRole));
         userRepository.save(user);
 
         return userMapper.toResponseDto(user);
