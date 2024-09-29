95bfbf7f9a77b54a9d1c776cdbcb9e38162e8c7c
ChabVlad <vlad.chabak@gmail.com>
2024-09-29 23:54:21 +0400

    shoppingCart

--- src/main/java/project/bookstore/dto/item/CreateCartItemDto.java
+++ src/main/java/project/bookstore/dto/item/CreateCartItemDto.java
@@ -1,5 +1,6 @@
 package project.bookstore.dto.item;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,6 +8,8 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CreateCartItemDto {
+    @NotNull
+    @Positive
     private Long bookId;
     @Positive
     private int quantity;
